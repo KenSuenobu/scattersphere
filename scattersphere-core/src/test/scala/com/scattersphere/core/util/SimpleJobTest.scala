db4b0ab5689dbfdbb896b97c993ff0482f084756
ksuenobu <ksuenobu@fastmail.com>
2018-04-21 16:12:53 -0500

    Updated to correct dependency path.

--- scattersphere-core/src/test/scala/com/scattersphere/core/util/SimpleJobTest.scala
+++ scattersphere-core/src/test/scala/com/scattersphere/core/util/SimpleJobTest.scala
@@ -35,6 +35,7 @@ class SimpleJobTest extends FlatSpec with Matchers {
     }
   }
 
+  // Task1 <- Task2
   it should "be able to add a secondary task as a dependency" in {
     val task1: TaskDesc = new TaskDesc("First Runnable Task", new RunnableTask1)
     val task2: TaskDesc = new TaskDesc("Second Runnable Task", new RunnableTask1)
@@ -42,9 +43,30 @@ class SimpleJobTest extends FlatSpec with Matchers {
     task1.taskName shouldBe "First Runnable Task"
     task2.taskName shouldBe "Second Runnable Task"
 
-    task1.addDependency(task2)
-    task1.getDependencies.length equals 1
-    task1.getDependencies.take(1) equals task2
+    // Adding task1 as a dependency on task2 means that task1 must complete before task2
+    // can start.
+    task2.addDependency(task1)
+    task2.getDependencies.length equals 1
+    task2.getDependencies.take(1) equals task1
+  }
+
+  // Task1 <- Task2 <- Task3
+  it should "be able to add a secondary and tertiary task as a dependency" in {
+    val task1: TaskDesc = new TaskDesc("First Runnable Task", new RunnableTask1)
+    val task2: TaskDesc = new TaskDesc("Second Runnable Task", new RunnableTask1)
+    val task3: TaskDesc = new TaskDesc("Third Runnable Task", new RunnableTask1)
+
+    task1.taskName shouldBe "First Runnable Task"
+    task2.taskName shouldBe "Second Runnable Task"
+    task3.taskName shouldBe "Third Runnable Task"
+
+    task3.addDependency(task2)
+    task3.getDependencies.length equals 1
+    task3.getDependencies.take(1) equals task2
+
+    task2.addDependency(task1)
+    task2.getDependencies.length equals 1
+    task2.getDependencies.take(1) equals task1
   }
 
 }
