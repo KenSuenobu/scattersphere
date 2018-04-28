9e0c7317ba439db508ff4b1214c3d6b237f6045d
ksuenobu <ksuenobu@fastmail.com>
2018-04-27 21:42:52 -0500

    Improved test.

--- scattersphere-core/src/test/scala/com/scattersphere/core/util/SimpleJobTest.scala
+++ scattersphere-core/src/test/scala/com/scattersphere/core/util/SimpleJobTest.scala
@@ -28,40 +28,53 @@ import org.scalatest.{FlatSpec, Matchers}
 class SimpleJobTest extends FlatSpec with Matchers  {
 
   class RunnableTestTask(name: String) extends Runnable {
+    var setVar: Int = 0
+
     def run(): Unit = {
       val sleepTime = 1000
 
       println(s"[$name] Sleeping $sleepTime milliseconds.")
       Thread.sleep(sleepTime)
       println(s"[$name] Sleep thread completed.")
+
+      setVar = name.toInt
     }
   }
 
   "Simple Jobs" should "prepare a job and execute the first and second task properly" in {
-    val task1: Task = new Task("First Runnable Task", new RunnableTestTask("1"))
-    val task2: Task = new Task("Second Runnable Task", new RunnableTestTask("2"))
-    val task3: Task = new Task("Third Runnable Task", new RunnableTestTask("3"))
+    val runnableTask1 = new RunnableTestTask("1")
+    val runnableTask2 = new RunnableTestTask("2")
+    val runnableTask3 = new RunnableTestTask("3")
+    val task1: Task = new Task("First Runnable Task", runnableTask1)
+    val task2: Task = new Task("Second Runnable Task", runnableTask2)
+    val task3: Task = new Task("Third Runnable Task", runnableTask3)
 
     task1.name shouldBe "First Runnable Task"
-    task1.getDependencies.length equals 0
+    task1.getDependencies.length shouldBe 0
 
     task2.name shouldBe "Second Runnable Task"
     task2.addDependency(task1)
-    task2.getDependencies.length equals 1
+    task2.getDependencies.length shouldBe 1
 
     task3.name shouldBe "Third Runnable Task"
     task3.addDependency(task2)
-    task3.getDependencies.length equals 1
+    task3.getDependencies.length shouldBe 1
 
     val job1: Job = new Job("Test", Seq(task1, task2, task3))
     val jobExec: JobExecutor = new JobExecutor(job1)
 
-    job1.tasks.length equals 3
-    job1.tasks(0) equals task1
-    job1.tasks(1) equals task2
-    job1.tasks(2) equals task3
+    job1.tasks.length shouldBe 3
+    job1.tasks(0) shouldBe task1
+    job1.tasks(1) shouldBe task2
+    job1.tasks(2) shouldBe task3
+    runnableTask1.setVar shouldBe 0
+    runnableTask2.setVar shouldBe 0
+    runnableTask3.setVar shouldBe 0
 
     jobExec.queue().join()
+    runnableTask1.setVar shouldBe 1
+    runnableTask2.setVar shouldBe 2
+    runnableTask3.setVar shouldBe 3
   }
 
 }
