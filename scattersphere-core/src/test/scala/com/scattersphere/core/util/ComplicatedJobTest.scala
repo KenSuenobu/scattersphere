b617aa45ab9553767d2206e58b61e0f16d302d7c
ksuenobu <ksuenobu@fastmail.com>
2018-04-29 22:32:59 -0500

    Added a more complicated DAG to the test suite.

--- scattersphere-core/src/test/scala/com/scattersphere/core/util/ComplicatedJobTest.scala
+++ scattersphere-core/src/test/scala/com/scattersphere/core/util/ComplicatedJobTest.scala
@@ -177,4 +177,100 @@ class ComplicatedJobTest extends FlatSpec with Matchers  {
     task4.getStatus shouldBe TaskStatus.FINISHED
   }
 
+  /**
+    * It creates a DAG like this, and is able to test:
+    *
+    *      /-> Task 2-A ---------------------\
+    * Task 1              -> Task 3-A-2-B -\  \
+    *      \-> Task 2-B -|                  /--> Task 4
+    *                     -> Task 3-B-2-B -/
+    */
+  it should "work with multiple dependencies" in {
+    val runnableTask1 = new RunnableTestTask("1")
+    val runnableTask2 = new RunnableTestTask("2-A")
+    val runnableTask3 = new RunnableTestTask("2-B")
+    val runnableTask4 = new RunnableTestTask("3-A-2-B")
+    val runnableTask5 = new RunnableTestTask("3-B-2-B")
+    val runnableTask6 = new RunnableTestTask("4")
+    val task1: Task = new Task("1", runnableTask1)
+    val task2: Task = new Task("2-A", runnableTask2)
+    val task3: Task = new Task("2-B", runnableTask3)
+    val task4: Task = new Task("3-A-2-B", runnableTask4, true)
+    val task5: Task = new Task("3-B-2-B", runnableTask5, true)
+    val task6: Task = new Task("4", runnableTask6)
+
+    task1.getStatus shouldBe TaskStatus.QUEUED
+    task2.getStatus shouldBe TaskStatus.QUEUED
+    task3.getStatus shouldBe TaskStatus.QUEUED
+    task4.getStatus shouldBe TaskStatus.QUEUED
+    task5.getStatus shouldBe TaskStatus.QUEUED
+    task6.getStatus shouldBe TaskStatus.QUEUED
+
+    task1.name shouldBe "1"
+    task1.getDependencies.length shouldBe 0
+
+    task2.name shouldBe "2-A"
+    task2.addDependency(task1)
+    task2.getDependencies.length shouldBe 1
+
+    task3.name shouldBe "2-B"
+    task3.addDependency(task1)
+    task3.getDependencies.length shouldBe 1
+
+    task4.name shouldBe "3-A-2-B"
+    task4.addDependency(task3)
+    task4.getDependencies.length shouldBe 1
+
+    task5.name shouldBe "3-B-2-B"
+    task5.addDependency(task3)
+    task5.getDependencies.length shouldBe 1
+
+    task6.name shouldBe "4"
+    task6.addDependency(task2)
+    task6.addDependency(task4)
+    task6.addDependency(task5)
+    task6.getDependencies.length shouldBe 3
+
+    val job1: Job = new Job("Test", Seq(task1, task2, task3, task4, task5, task6))
+    val jobExec: JobExecutor = new JobExecutor(job1)
+
+    job1.tasks.length shouldBe 6
+    job1.tasks(0) shouldBe task1
+    job1.tasks(1) shouldBe task2
+    job1.tasks(2) shouldBe task3
+    job1.tasks(3) shouldBe task4
+    job1.tasks(4) shouldBe task5
+    job1.tasks(5) shouldBe task6
+    runnableTask1.setVar shouldBe ""
+    runnableTask2.setVar shouldBe ""
+    runnableTask3.setVar shouldBe ""
+    runnableTask4.setVar shouldBe ""
+    runnableTask5.setVar shouldBe ""
+    runnableTask6.setVar shouldBe ""
+
+    jobExec.queue().join()
+    runnableTask1.setVar shouldBe "1"
+    runnableTask2.setVar shouldBe "2-A"
+    runnableTask3.setVar shouldBe "2-B"
+    runnableTask4.setVar shouldBe "3-A-2-B"
+    runnableTask5.setVar shouldBe "3-B-2-B"
+    runnableTask6.setVar shouldBe "4"
+
+    // Since we have a job that splits from 1 task to 2, then to 1, we want to make sure the last task doesn't
+    // get erroneously called twice!
+    runnableTask1.callCount.get() shouldBe 1
+    runnableTask2.callCount.get() shouldBe 1
+    runnableTask3.callCount.get() shouldBe 1
+    runnableTask4.callCount.get() shouldBe 1
+    runnableTask5.callCount.get() shouldBe 1
+    runnableTask6.callCount.get() shouldBe 1
+
+    task1.getStatus shouldBe TaskStatus.FINISHED
+    task2.getStatus shouldBe TaskStatus.FINISHED
+    task3.getStatus shouldBe TaskStatus.FINISHED
+    task4.getStatus shouldBe TaskStatus.FINISHED
+    task5.getStatus shouldBe TaskStatus.FINISHED
+    task6.getStatus shouldBe TaskStatus.FINISHED
+  }
+
 }
