ece56d9aaec2b587628dd373167b3c457d8e7e33
ksuenobu <ksuenobu@fastmail.com>
2018-05-21 23:54:00 -0500

    Added ability to cancel a job.

---
+++ scattersphere-core/src/test/scala/com/scattersphere/core/util/CancelJobTest.scala
@@ -0,0 +1,54 @@
+package com.scattersphere.core.util
+
+import com.scattersphere.core.util.execution.JobExecutor
+import com.typesafe.scalalogging.LazyLogging
+import org.scalatest.{FlatSpec, Matchers}
+
+class CancelJobTest extends FlatSpec with Matchers with LazyLogging {
+
+  class SleeperRunnable(time: Int) extends Runnable {
+    override def run(): Unit = {
+      logger.info(s"Sleeping $time second(s).")
+      Thread.sleep(time * 1000)
+      logger.info("Sleep complete.")
+    }
+  }
+
+  "Job Executor" should "be able to cancel a list of tasks" in {
+    val task1: Task = TaskBuilder().withTask(RunnableTask(new SleeperRunnable(1))).withName("1").build()
+    val task2: Task = TaskBuilder().withName("2").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task1).build()
+    val task3: Task = TaskBuilder().withName("3").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task2).build()
+    val task4: Task = TaskBuilder().withName("4").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task3).build()
+    val task5: Task = TaskBuilder().withName("5").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task4).build()
+    val task6: Task = TaskBuilder().withName("6").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task5).build()
+    val task7: Task = TaskBuilder().withName("7").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task6).build()
+    val task8: Task = TaskBuilder().withName("8").withTask(RunnableTask(new SleeperRunnable(1))).dependsOn(task7).build()
+    val job1: Job = JobBuilder().withName("Timer Task").withTasks(task1, task2, task3, task4, task5, task6, task7, task8).build()
+    val jobExec: JobExecutor = JobExecutor(job1)
+
+    jobExec.setBlocking(false)
+    jobExec.queue().run()
+    Thread.sleep(2500)
+    jobExec.cancel("Canceling for testing sake.")
+
+    Thread.sleep(2000)
+
+    job1.status match {
+      case JobCanceled(x) => x shouldBe "Canceling for testing sake."
+      case x => fail(s"Unexpected job status matched: $x")
+    }
+
+    var taskFinished: Int = 0
+    var taskCanceled: Int = 0
+
+    job1.tasks.foreach(task => task.status() match {
+      case TaskCanceled(_) => taskCanceled += 1
+      case TaskFinished => taskFinished += 1
+      case _ => // Ignored
+    })
+
+    assert(taskFinished > 0)
+    assert(taskCanceled > 0)
+  }
+
+}
