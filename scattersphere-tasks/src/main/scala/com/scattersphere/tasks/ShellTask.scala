83f3da254f6de6a32435ea3e3ffbae3ad618ec7a
ksuenobu <ksuenobu@fastmail.com>
2018-05-26 13:21:15 -0500

    Added changes to ShellTask, now tests failures, fixed a bug with JobFailed notification.

--- scattersphere-tasks/src/main/scala/com/scattersphere/tasks/ShellTask.scala
+++ scattersphere-tasks/src/main/scala/com/scattersphere/tasks/ShellTask.scala
@@ -5,20 +5,30 @@ import com.typesafe.scalalogging.LazyLogging
 
 import scala.sys.process.{Process, ProcessBuilder}
 
+/** Executes a Shell command on the command line.  These commands must terminate at some point.  There is no timeout
+  * delay for the task, so if a failure happens on the Shell task and it hangs, the underlying job controller must
+  * terminate the task manually.
+  *
+  * @constructor creates a new ShellTask with the command to execute
+  * @param command command string to execute, options all separated by spaces
+  * @since 0.1.0
+  */
 class ShellTask(command: String) extends RunnableTask with LazyLogging {
 
   private var processBuilder: ProcessBuilder = null
-  private var processStream: Stream[String] = null
   private var process: Process = null
 
   override def run(): Unit = {
     logger.info(s"Running command ${command}")
     processBuilder = Process(command)
-    processStream = processBuilder.lineStream
     process = processBuilder.run
   }
 
-  def getProcessOutput(): Stream[String] = processStream
+  /** Returns the output (stdout) from the process as a stream of [[String]] data.
+    *
+    * @return stream of string objects
+    */
+  def getProcessOutput(): Stream[String] = processBuilder.lineStream
 
   override def onFinished(): Unit = {
     logger.info("Command finished.")
@@ -32,7 +42,7 @@ class ShellTask(command: String) extends RunnableTask with LazyLogging {
   }
 
   override def onException(t: Throwable): Unit = {
-    logger.info(s"Exception occurred during run: ${t}")
+    logger.info("Exception occurred during run", t)
 
     if (process.isAlive()) {
       logger.info("Process is still running; terminating.")
--- scattersphere-tasks/src/test/scala/com/scattersphere/tasks/ShellTaskTest.scala
+++ scattersphere-tasks/src/test/scala/com/scattersphere/tasks/ShellTaskTest.scala
@@ -1,4 +1,20 @@
+/**
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
 package com.scattersphere.tasks
+
+import java.util.concurrent.CompletionException
 
 import com.scattersphere.core.util.execution.JobExecutor
 import com.scattersphere.core.util._
@@ -20,6 +36,25 @@ class ShellTaskTest extends FlatSpec with Matchers with LazyLogging {
 
     assert(output.size > 2)
     assert(output(0).toLowerCase() contains "total")
+  }
+
+  it should "handle commands that do not exist" in {
+    val shellTask: ShellTask = new ShellTask("/unable/to/find/this command anywhere")
+    val sTask1: Task = TaskBuilder().withName("nonexistent task").withTask(shellTask).build()
+    val job: Job = JobBuilder().withTasks(sTask1).build()
+    val jExec: JobExecutor = JobExecutor(job)
+
+    assertThrows[CompletionException] {
+      jExec.queue().run()
+    }
+
+    job.status match {
+      case JobFailed(x) => x match {
+        case _: java.io.IOException => // Expected exception as well
+      }
+
+      case x => fail(s"Unexpected job status: $x")
+    }
   }
 
 }
