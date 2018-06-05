bee06d4e6f31e52f25da91d37a77efb507d5d048
ksuenobu <ksuenobu@fastmail.com>
2018-06-05 00:32:12 -0500

    Initial scripts for docker support in CircleCI/Local

---
+++ scripts/circle/start-docker.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Script to start up the Spark 2.3.0 Docker Container
+
+docker pull mesosphere/spark
+
---
+++ scripts/circle/stop-docker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#
+# Script to shut down the Spark 2.3.0 Docker Container
+
+
