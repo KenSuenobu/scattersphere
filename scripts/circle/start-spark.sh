13ddf1763125c90c34f6e852e48d9b57257984ae
ksuenobu <ksuenobu@fastmail.com>
2018-06-06 20:15:57 -0500

    Updated start spark script.

--- scripts/circle/start-spark.sh
+++ scripts/circle/start-spark.sh
@@ -3,8 +3,8 @@
 # CircleCI script used to start a Spark instance
 # This script should not be used outside of the CircleCI network; this is just for CircleCI testing.
 
-wget http://www-us.apache.org/dist/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz -qO /tmp/spark-2.3.0.tar.gz
-cd /opt ; tar -xvzf /tmp/spark-2.3.0.tar.gz
-rm -f /tmp/spark-2.3.0.tar.gz
-cd /opt ; mv spark-* apache-spark-2.3.0
-cd /opt/apache-spark-2.3.0/sbin ; sh ./start-master.sh
+sudo wget http://www-us.apache.org/dist/spark/spark-2.3.0/spark-2.3.0-bin-hadoop2.7.tgz -qO /tmp/spark-2.3.0.tar.gz && \
+     cd /opt ; tar -xvzf /tmp/spark-2.3.0.tar.gz && \
+     rm -f /tmp/spark-2.3.0.tar.gz && \
+     cd /opt ; mv spark-* apache-spark-2.3.0 && \
+     cd /opt/apache-spark-2.3.0/sbin ; sh ./start-master.sh
