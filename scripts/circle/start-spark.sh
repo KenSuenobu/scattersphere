57e197d633fa56bb7035689808c44817e81d722d
ksuenobu <ksuenobu@fastmail.com>
2018-06-06 20:32:24 -0500

    Forgot to start a slave along with the master.

--- scripts/circle/start-spark.sh
+++ scripts/circle/start-spark.sh
@@ -10,4 +10,5 @@ rm -f spark-2.3.0.tar.gz
 mv spark-2.3.0-bin-hadoop2.7 spark-2.3.0
 cd spark-2.3.0/sbin
 sh ./start-master.sh -h 127.0.0.1
+sh ./start-slave.sh -c 2 -m 8G spark://127.0.0.1:7077
 cd -
