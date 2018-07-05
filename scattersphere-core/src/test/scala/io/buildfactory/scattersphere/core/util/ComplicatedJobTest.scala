003e32dab409bbd060f5b24eae0d4ddaeb500910
ksuenobu <ksuenobu@fastmail.com>
2018-07-04 22:09:28 -0500

    Updated license message at the top of each project.

--- scattersphere-core/src/test/scala/io/buildfactory/scattersphere/core/util/ComplicatedJobTest.scala
+++ scattersphere-core/src/test/scala/io/buildfactory/scattersphere/core/util/ComplicatedJobTest.scala
@@ -1,16 +1,23 @@
-/**
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+/*
+ *    _____            __  __                       __
+ *   / ___/_________ _/ /_/ /____  ______________  / /_  ___  ________
+ *   \__ \/ ___/ __ `/ __/ __/ _ \/ ___/ ___/ __ \/ __ \/ _ \/ ___/ _ \
+ *  ___/ / /__/ /_/ / /_/ /_/  __/ /  (__  ) /_/ / / / /  __/ /  /  __/
+ * /____/\___/\__,_/\__/\__/\___/_/  /____/ .___/_/ /_/\___/_/   \___/
+ *                                       /_/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.buildfactory.scattersphere.core.util
 
 import java.util.concurrent.atomic.AtomicInteger
