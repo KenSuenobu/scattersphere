be4bfd54a37896d8b04c3cd1ef94472bb45e93d8
Kenji Suenobu <kenji@Kenjis-MacBook-Pro.local>
2018-11-04 21:37:14 -0800

    Spark 2.3.2

--- scripts/circle/start-spark.sh
+++ scripts/circle/start-spark.sh
@@ -3,12 +3,17 @@
 # CircleCI script used to start a Spark instance
 # This script should not be used outside of the CircleCI network; this is just for CircleCI testing.
 
+VERSION="2.3.2"
+
+echo "Downloading Spark ${VERSION}"
+echo
+
 cd ~
-wget http://www-us.apache.org/dist/spark/spark-2.3.1/spark-2.3.1-bin-hadoop2.7.tgz -qO ./spark-2.3.1.tar.gz
-tar -xvzf ./spark-2.3.1.tar.gz
-rm -f spark-2.3.1.tar.gz
-mv spark-2.3.1-bin-hadoop2.7 spark-2.3.1
-cd spark-2.3.1/sbin
+wget http://www-us.apache.org/dist/spark/spark-${VERSION}/spark-${VERSION}-bin-hadoop2.7.tgz -qO ./spark-${VERSION}.tar.gz
+tar -xvzf ./spark-${VERSION}.tar.gz
+rm -f spark-${VERSION}.tar.gz
+mv spark-${VERSION}-bin-hadoop2.7 spark-${VERSION}
+cd spark-${VERSION}/sbin
 bash ./start-master.sh -h 127.0.0.1
 bash ./start-slave.sh -c 2 -m 8G spark://127.0.0.1:7077
 cd -
