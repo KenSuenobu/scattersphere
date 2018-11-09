9445f90a0ed16220dd83169751870085c2b64853
Kenji Suenobu <kenji@Kenjis-MacBook-Pro.local>
2018-11-09 13:55:28 -0800

    Updated to include host and port on startup.

--- scripts/circle/start-spark.sh
+++ scripts/circle/start-spark.sh
@@ -4,6 +4,8 @@
 # This script should not be used outside of the CircleCI network; this is just for CircleCI testing.
 
 VERSION="2.4.0"
+HOST="127.0.0.1"
+PORT="7077"
 
 echo "Downloading Spark ${VERSION}"
 echo
@@ -14,6 +16,10 @@ tar -xvzf ./spark-${VERSION}.tar.gz
 rm -f spark-${VERSION}.tar.gz
 mv spark-${VERSION}-bin-hadoop2.7 spark-${VERSION}
 cd spark-${VERSION}/sbin
-bash ./start-master.sh -h 127.0.0.1
-bash ./start-slave.sh -c 2 -m 8G spark://127.0.0.1:7077
+
+echo
+echo "Starting Spark: Host=${HOST} Port=${PORT}"
+echo
+bash ./start-master.sh -h ${HOST} -p ${PORT}
+bash ./start-slave.sh -c 2 -m 8G spark://${HOST}:${PORT}
 cd -
